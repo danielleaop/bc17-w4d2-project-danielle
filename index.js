cd594299f037597208b73470c58c72178df39b99
Danielle <168420921+danielleaop@users.noreply.github.com>
2024-07-23 14:00:26 +0100

    us2

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import express from "express";
+import helmet from "helmet";
+
 const app = express();
+
+// Use Helmet!
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
