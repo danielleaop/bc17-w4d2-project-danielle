cf69587f1d73efb0ba1501791355b99eef571b74
Dionne Memie <d.memie@gmail.com>
2024-07-23 12:23:09 +0100

    Epic 1: User Story 1 completed

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-import express from 'express';
+import express from "express";
 const app = express();
-let statusCode = res.sendStatus();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  res.sendStatus = 200;
-  console.log(statusCode);
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
 });
 
-// Adding new routes is as simple as adding new app.get() calls
 app.listen(3000, () => {
-  console.log('Request logged.');
+  console.log("Request logged.");
 });
