9b8565bb9fae5bdbb417bbc364ab419aa0edac45
Danielle <168420921+danielleaop@users.noreply.github.com>
2024-07-24 14:40:12 +0100

    pre tom talk

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import helmet from "helmet";
 import info from "./userActivity.json" with {type: "json"};
+import { v4 as uuidv4 } from "uuid";
+
 const app = express();
-// Use Helmet!
+
+
 app.use(helmet());
 
 app.get("/", (req, res) => {
@@ -17,6 +20,13 @@ app.get("/activities", (req, res) => {
     }
   );
 });
+
+app.post("/activities", (req, res) => {
+  //try
+
+
+  //catch
+})
 
 app.listen(3000, () => {
   console.log("Request logged.");
